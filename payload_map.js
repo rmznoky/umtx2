b61b205633cd45b9dffd31aecf414d9454172237
rmznoky <sitemkar.65@hotmail.com>
2026-05-18 00:09:05 +0300

    Update payload_map.js

--- payload_map.js
+++ payload_map.js
@@ -42,12 +42,12 @@ const payload_map = [
     },
     {
         displayTitle: "ps5-kstuff-lite",
-        description: "FPKG enabler",
+        description: "Kstuff Lite 1.02",
         fileName: "kstuff.elf",
         author: "sleirsgoevy, john-tornblom, EchoStretch, buzzer-re, BestPig, LightningMods, zecoxao, idlesauce",
         projectSource: "https://github.com/EchoStretch/kstuff-lite",
         binarySource: "https://github.com/EchoStretch/kstuff-lite/releases/download/v1.03/",
-        version: "1.03",
+        version: "1.02",
         supportedFirmwares: ["3.", "4.", "5.", "6.", "7.", "8."],
         toPort: 9021
     },
